b45dc3eefe4ad448ff3be4e13445a5d596e61153
bpv834 <81566093+bpv834@users.noreply.github.com>
2021-05-23 17:36:08 +0900

    초기 커밋.

---
+++ 참조에 의한 호출로 구조체 전달/참조에 의한 호출로 구조체 전달.cpp
@@ -0,0 +1,21 @@
+﻿#include<stdio.h>
+#include<string.h>
+
+struct content {
+	char title[40];
+	int price;
+	double rate;
+};
+
+int main() {
+	struct content c1 = { "aaa",1000,10.0 };
+	struct content* p = &c1;
+	printf("%s,%d,%.1f\n", c1.title, c1.price, c1.rate);
+
+	p->price *= 2;
+	p->rate = 10.5;
+	strcat_s(p->title, ":endgame");
+
+	printf("%s,%d,%.1f\n", p->title, p->price, p->rate);
+	printf("%s,%d,%.1f\n", c1.title, c1.price, c1.rate);
+}
